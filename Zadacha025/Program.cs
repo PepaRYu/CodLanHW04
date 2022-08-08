d54539c9061bf75d15ee8537e1752ce785afd18a
DrPRun <sheighd@gmail.com>
2022-08-08 22:19:47 +0300

    Zadacha025

--- Zadacha025/Program.cs
+++ Zadacha025/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите основание: ");
+int bace = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int exponent = Convert.ToInt32(Console.ReadLine());
+int stepen = 1;
+for (int i = 0; i < exponent; i++)
+{
+    stepen *= bace;
+}
+
+Console.WriteLine(stepen);
