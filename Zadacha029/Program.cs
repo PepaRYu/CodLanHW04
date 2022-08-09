973b72fddda8bded2f4b0465864a13025871a018
DrPRun <sheighd@gmail.com>
2022-08-09 13:26:07 +0300

    zadacha029 reshena

---
+++ Zadacha029/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] GetBinaryArray(int size)
+{
+    int [] result = new int[size];
+    // result.Length = size
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(10);
+    }
+    return result;
+
+}
+
+int [] array = GetBinaryArray(8);
+Console.Write($"[{String.Join(",", array)}]");
